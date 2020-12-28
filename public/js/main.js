4e357f56b433e4d4bf75a805c0e4d94fb26bfcd7
Achraf <hardy1348@live.fr>
2020-12-28 17:17:04 +0100

    fin

---
+++ public/js/main.js
@@ -0,0 +1,21 @@
+let chiffre1 = 10;
+let chiffre2 = 2;
+let resultat = 12;
+console.log(`${chiffre1} + ${chiffre2} = ${resultat} `);
+let resultat1 = 8;
+console.log(`${chiffre1} - ${chiffre2} = ${resultat1} `);
+let resultat2 = 5;
+console.log(`${chiffre1} : ${chiffre2} = ${resultat2} `);
+let resultat3 = 20;
+console.log(`${chiffre1} x ${chiffre2} = ${resultat3} `);
+
+let age = prompt("Ton age ?")
+let prenom = prompt("Ton prenom ?")
+console.log(prenom + age );
+let age2 = "25 ans"
+
+let phrase = `${prenom}a ${age}cette annee`;
+console.log(phrase);
+
+let phrase1 = `${prenom}aura ${age2} dans 3 ans`
+console.log(phrase1);
